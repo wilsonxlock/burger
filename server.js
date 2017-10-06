14dbf2aa438549a284ab042781ebb9332dfb5abe
Tyler Wilson <x@tylers-mbp.attlocal.net>
2017-10-06 08:59:46 -0400

    cant figure out 404 error, its posting to db but not displaying anything

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
-var app = express();
+const port = 8080;
+const app = express();
 const mysql = require("mysql");
 app.use(bodyParser.urlencoded({
     extended: false
@@ -12,3 +13,28 @@ app.engine("handlebars", exphbs({
     defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("connected as id " + connection.threadId);
+});
+app.post("/", function (req, res) {
+
+    connection.query("INSERT INTO burgers (burger_type) VALUES (?)", [req.body.burgers], function (err, result) {
+        if (err) throw err;
+
+        res.redirect("/");
+    });
+});
+
+app.listen(port);
